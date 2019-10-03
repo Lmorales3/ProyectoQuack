e1f6d25c1785cb9f7a2162b6c98faf3b40e8d4a9
valehp <valenthernandez@udec.cl>
2019-10-02 22:22:04 -0300

    Arreglado usuario

--- app/src/main/java/com/example/proyectoquack/Entidades/Usuario.java
+++ app/src/main/java/com/example/proyectoquack/Entidades/Usuario.java
@@ -1,78 +1,61 @@
 package com.example.proyectoquack.Entidades;
 
 import java.io.Serializable;
+import java.util.*;
 
 public class Usuario implements Serializable {
     private String username;
     private String nombre;
     private String password;
     private String correo;
-    private String telefono;
-    private String rut;
-    private String sexo;
-    public Usuario (String username, String nombre, String password, String correo, String telefono, String rut, String sexo){
+    private String fecha_nacimiento;
+    private String carrera;
+    private Vector almuerzos_preferidos;
+    private float quack_puntos;
+    public Usuario (String username, String nombre, String password, String correo, Vector almuerzos,
+                    String fecha_nacimiento, String carrera, float quack_puntos){
         this.username=username;
         this.nombre=nombre;
         this.password=password;
         this.correo=correo;
-        this.telefono=telefono;
-        this.rut=rut;
-        this.sexo=sexo;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.carrera = carrera;
+        this.quack_puntos = quack_puntos;
+        this.almuerzos_preferidos = (Vector)almuerzos.clone();
     }
 
     public String getUsername(){
         return username;
     }
-
     public String getNombre() {
         return nombre;
     }
-
     public String getPassword() {
         return password;
     }
-
     public String getCorreo() {
         return correo;
     }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public String getRut() {
-        return rut;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
+    public Vector getAlmuerzos_preferidos(){ return almuerzos_preferidos; }
+    public String getFecha_nacimiento(){ return  fecha_nacimiento; }
+    public String getCarrera(){ return carrera; }
+    public float getQuack_puntos(){ return  quack_puntos; }
 
     public void setUsername(String username) {
         this.username = username;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public void setCorreo(String correo) {
         this.correo = correo;
     }
+    public void setFecha_nacimiento(String fecha){ this.fecha_nacimiento = fecha; }
+    public void setCarrera(String c){ this.carrera = c; }
+    public void setAlmuerzos_preferidos(Vector a){ this.almuerzos_preferidos = (Vector)a.clone(); }
+    public void setQuack_puntos(float puntos){ this.quack_puntos = puntos; }
 
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public void setRut(String rut) {
-        this.rut = rut;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
 }
