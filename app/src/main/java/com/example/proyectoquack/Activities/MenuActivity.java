e5089912321425af5aa08618ab27b6efb5054a31
valehp <valenthernandez@udec.cl>
2019-10-11 00:42:30 -0300

    pequeños cambios en xml

--- app/src/main/java/com/example/proyectoquack/Activities/MenuActivity.java
+++ app/src/main/java/com/example/proyectoquack/Activities/MenuActivity.java
@@ -1,7 +1,20 @@
 package com.example.proyectoquack.Activities;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
 
+        protected void onCreate(){
+
+        }
+
+        public void onClickJuna(View view){
+            Toast toast1 = Toast.makeText(getApplicationContext(), "Prueba JUNA", Toast.LENGTH_SHORT);
+           // toast1.setGravity(Gravity.CENTER);
+
+            toast1.show();
+        }
 }
